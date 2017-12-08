f72317eccc785a766f5aaa6aaaee0fff5f5026f8
cvonmorze <cvonmorze@hotmail.com>
2017-12-08 14:43:17 -0800

    Add files via upload

---
+++ closeGPIB.m
@@ -0,0 +1,9 @@
+% closeGPIB.m
+%
+% This code closes the GPIB connection.
+%
+% Author:  Cornelius von Morze
+%
+% (c)2017 The Regents of the University of California. All Rights Reserved.
+
+agt_closeAllSessions
